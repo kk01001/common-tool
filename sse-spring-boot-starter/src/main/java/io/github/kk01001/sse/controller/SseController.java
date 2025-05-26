af4c5f326473e1f3d8f5b870c4917a936521f6c6
linshiqiang <linshiqiang@linkcircle.cn>
2025-05-26 15:26:34 +0800

    feat: [dev-2.4.6.5] [excel] add BigDataCsvlExporter

--- sse-spring-boot-starter/src/main/java/io/github/kk01001/sse/controller/SseController.java
+++ sse-spring-boot-starter/src/main/java/io/github/kk01001/sse/controller/SseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 /**
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
  * @description SSE控制器
  */
 @Slf4j
-@RestController
+@ResponseBody
 @RequestMapping("/sse")
 @RequiredArgsConstructor
 public class SseController {
